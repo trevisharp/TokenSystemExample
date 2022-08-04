64af74b29ca33b439989ebc090b6349278e34656
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2022-08-04 11:19:09 -0300

    Implementação da interface

---
+++ blazorExemplo/Data/ConnectMessage.cs
@@ -0,0 +1,5 @@
+public class ConnectMessage
+{
+    public string Message { get; set; }
+    public string Token { get; set; }
+}
--- blazorExemplo/Program.cs
+++ blazorExemplo/Program.cs
@@ -9,6 +9,13 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
+builder.Services.AddSingleton<HttpClient>(p =>
+{
+    var client = new HttpClient();
+    client.BaseAddress = new Uri("https://localhost:7190/api/");
+    return client;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
